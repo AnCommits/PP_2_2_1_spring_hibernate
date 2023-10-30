6c143f729620590aab072458acda92666c4a09e7
AnUser99 <kisamasyanya@gmail.com>
2023-10-30 13:49:32 +0300

    Modified getRandomCar, Added public Car(@NonNull String model).

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -2,17 +2,15 @@ package hiber;
 
 import hiber.config.AppConfig;
 import hiber.model.Car;
-import hiber.model.CarModel;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainApp {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -6,10 +6,16 @@ import java.util.List;
 
 public interface UserService {
     void add(User user);
+
     List<User> listUsers();
+
     User getUserByModelAndSeries(String model, int series);
+
     User getUser(long id);
+
     void remove(User user);
+
     void update(User user);
+
     boolean remove(long id);
 }
